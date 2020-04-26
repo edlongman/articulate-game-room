10edb113c9fca395b96412d519e87849585ff3ed
Edward Longman <eialongman@gmail.com>
2020-04-26 15:40:51 +0100

    Pull util functions out of main app

--- app.js
+++ app.js
@@ -3,39 +3,7 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const util = require('util');
-
-function makeid(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
-}
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const {makeId, shuffle, getRandomInt} = require('./gameUtil');
 
 const fileLen = 10;
 const extMap = {"image/png": 'png', "image/jpeg": 'jpeg'}
@@ -49,7 +17,7 @@ const storage = multer.diskStorage({
       cb("Illegal upload type");
       return;
     }
-    cb(null, makeid(fileLen) + '.'+ extension)
+    cb(null, makeId(fileLen) + '.'+ extension)
   }
 });
 const upload = multer({ storage: storage,
---
+++ gameUtil.js
@@ -0,0 +1,34 @@
+
+exports.makeId = function makeId(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+exports.shuffle = function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+exports.getRandomInt = function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
