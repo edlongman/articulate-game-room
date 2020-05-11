99d054690185312604579396443146ec0fef9138
Edward Longman <eialongman@gmail.com>
2020-05-11 13:55:50 +0100

    Rotate the jpeg images uploaded to the admin

--- imageStore.js
+++ imageStore.js
@@ -1,7 +1,8 @@
 'use strict';
 const multer = require('multer');
+const jpegRot = require('jpeg-autorotate');
 const util = require('util');
-const fs = require('fs');
+const fs = require('fs').promises;
 const {makeId} = require('./gameUtil');
 const express = require('express');
 const asyncUnlink = util.promisify(fs.unlink);
@@ -23,16 +24,53 @@ const storage = multer.diskStorage({
     cb(null, makeId(fileLen) + '.'+ extension)
   }
 });
-const upload = multer({ storage: storage,
-  limits:{fileSize: 10000000} });
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 10000000},
+  fileFilter: function(req, file, cb){
+    const extension = extMap[file.mimetype];
+    if(extension == null){ //TODO: Check if this can be fooled
+      cb(null, false);
+      return;
+    }
+    cb(null, true);
+  }
+});
 const cardUpload = upload.array('cards', 20);
 function finishUpload(req, res, next) {
   req.imageUpload = true; //Mark as successful upload
-  // Record all uploads to empty them when finished
-  instance_upload_list = instance_upload_list.concat(
-    req.files.map((item) => item.path)
-  );
-  next();
+  // Fix rotation on jpegs
+  Promise.all(req.files.map((file)=>{
+    if(file.mimetype=="image/jpeg"){
+      return jpegRot.rotate(file.path,{})
+        .then(function (new_image){
+          // Overwrite the original file
+          return fs.writeFile(file.path, new_image.buffer);
+        })
+        .catch((err)=>{
+          switch(err.code){
+            case jpegRot.errors.read_exif:
+            case jpegRot.errors.no_orientation:
+            case jpegRot.errors.unknown_orientation:
+            case jpegRot.errors.correct_orientation:
+              // Ignore error and leave image unchanged
+              break;
+            default:
+              console.log(err);
+              throw err;
+              break;
+          }
+        });
+    }
+    return true;
+  })).then(()=>{
+    // Record all uploads to empty them when finished
+    instance_upload_list = instance_upload_list.concat(
+      req.files.map((item) => item.path)
+    );
+
+    next();
+  });
 }
 
 
